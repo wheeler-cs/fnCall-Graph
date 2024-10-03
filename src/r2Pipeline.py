4318bf88a3f23abc03b5d87e3a4763e0a1765a59
wheeler-cs <wheelercs@protonmail.com>
2024-10-03 11:09:37 -0500

    Working on Graph Stuff
    Signed-off-by: wheeler-cs <wheelercs@protonmail.com>

--- src/r2Pipeline.py
+++ src/r2Pipeline.py
@@ -1,9 +1,42 @@
+# == Imports ===========================================================================================================
+import json
 import r2pipe
+from sys import argv
+from typing import Dict, List
 
-if __name__ == "__main__":
-    cmdPipe = r2pipe.open("NPP.exe")
+from graphNode import GraphNode
+
+
+
+# == Function Definitions ==============================================================================================
+def extractData(inputFile: str):
+    cmdPipe = r2pipe.open("data/NPP.exe")
     cmdPipe.cmd("aaa")
-    cmdPipe.cmd("agCd > output.dot")
-    cmdPipe.cmd("!!dot -Tpng -o callgraph.png output.dot")
+    cmdPipe.cmd("agCj > data/output.json")
 
+
+def readJson(inputFile: str):
+    with open(inputFile, 'r') as jsonFile:
+        jsonData = json.load(jsonFile)
+    return jsonData
+
+
+def buildGraph(jsonData) -> GraphNode:
+    rootNode = GraphNode("root", True)
+    for data in jsonData:
+        newNode = GraphNode(data["name"])
+        for child in data["imports"]:
+            childNode = GraphNode(child)
+            newNode.addChild(childNode)
+        rootNode.addChild(newNode)
+    return(rootNode)
+
+
+
+# == Main ==============================================================================================================
+if __name__ == "__main__":
+    jsonData = readJson(argv[1])
+
+    graph = buildGraph(jsonData)
+    graph.inorderTraversal()
 
